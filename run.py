a8b92b02e19f5c06c0ec1ef34419b5446dd23913
Isabelle Tot <isabelle.tot@un.org>
2024-09-04 23:58:51 +0100

    Adding test location for lookup yaml

--- run.py
+++ run.py
@@ -183,7 +183,8 @@ if __name__ == "__main__":
     print()
     facade(
         main,
-        user_agent_config_yaml=join("/Workspace/Shared/init-scripts/useragents.yaml"),
+        user_agent_config_yaml="/Workspace/Shared/init-scripts/.useragents.yaml",
+        hdx_config_yaml="/Workspace/Shared/init-scripts/.hdx_configuration.yaml",
         user_agent_lookup=lookup,
         project_config_yaml=join("config", "project_configuration.yaml"),
 
